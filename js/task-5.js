27dc9799f9f41d1d3beb44be37a7d8a4bb697b78
Natali Natalushko <natalinatalushko@gmail.com>
2026-06-18 00:35:43 +0200

    task-5

---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const button = document.querySelector('button.change-color');
+const colorSpan = document.querySelector('span.color');
+
+button.addEventListener('click', () => {
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+});
